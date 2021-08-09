b8371eb9f6276c446b48268aab200b2265a338da
Nick <ncalfano35@gmail.com>
2021-08-08 20:37:07 -0400

    NPC/Enemy Movement
    NPC/Enemy Movement

--- ProjectLostKin2D/Assets/Scripts/NPC.cs
+++ ProjectLostKin2D/Assets/Scripts/NPC.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
     public string npcName;
     public DialogueTrigger npcDialogue;
+    public float EnemySpeed;
+    public bool XMove;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,15 @@ public class NPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(XMove)
+        {
+            transform.Translate(2 * Time.deltaTime * EnemySpeed, 0, 0);
+            transform.localScale = new Vector2(2,2);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * EnemySpeed, 0, 0);
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -24,6 +35,19 @@ public class NPC : MonoBehaviour
         {
             Debug.Log("I'm triggered");
             npcDialogue.TriggerDialogue();
+        }
+
+        if (other.CompareTag("Turn"))
+        {
+            if(XMove)
+            {
+                XMove = false;
+            }
+
+            else
+            {
+                XMove = true;
+            }
         }
     }
     
