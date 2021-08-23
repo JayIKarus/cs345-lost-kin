d82c1c130d12d337999c0d5da9c97668bb79a3ac
Nick <ncalfano35@gmail.com>
2021-08-22 23:42:17 -0400

    Player and Enemy Health
    Basis for all player and enemy health related systems.

---
+++ ProjectLostKin2D/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Security.Principal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public int maxPlayerHealth;
+    public static int playerHealth;
+
+    Text text;
+
+    private LevelManager levelManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        playerHealth = maxPlayerHealth;
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHealth <= 0)
+        {
+            levelManager.RespawnPlayer();
+        }
+        text.text = "" + playerHealth;
+    }
+
+    public static void HurtPlayer(int damageToGive)
+    {
+        playerHealth -= damageToGive;
+    }
+
+    public void FullHealth()
+    {
+        playerHealth = maxPlayerHealth;
+    }
+}
--- ProjectLostKin2D/Assets/Scripts/LevelManager.cs
+++ ProjectLostKin2D/Assets/Scripts/LevelManager.cs
@@ -7,10 +7,12 @@ public class LevelManager : MonoBehaviour
 {
     public GameObject currCheckpoint;
     private PlayerMovement player;
+    public HealthManager healthManager;
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerMovement>();
+        healthManager = FindObjectOfType<HealthManager>();
     }
 
     // Update is called once per frame
@@ -22,5 +24,6 @@ public class LevelManager : MonoBehaviour
     public void RespawnPlayer()
     {
         player.transform.position = currCheckpoint.transform.position;
+        healthManager.FullHealth();
     }
 }
