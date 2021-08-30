889d3d9f766bc6de0af8d908afa7754c1d1bf870
jmk555@tux.cs.drexel.edu <61361783+JayIKarus@users.noreply.github.com>
2021-08-30 05:04:18 -0400

    committed Health stuff

--- ProjectLostKin2D/Assets/Scripts/HurtPlayer.cs
+++ ProjectLostKin2D/Assets/Scripts/HurtPlayer.cs
@@ -5,23 +5,35 @@ using UnityEngine;
 public class HurtPlayer : MonoBehaviour
 {
     public int damageToGive;
+    public static int playerHealth;
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void Awake()
+    {
+        heart1 = GameObject.Find("HeartContainer_1");
+        heart2 = GameObject.Find("HeartContainer_2");
+        heart3 = GameObject.Find("HeartContainer_3");
+    }
+
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.name == "Player")
         {
-            HealthManager.HurtPlayer(damageToGive);
+            HealthManager.HurtPlayer(damageToGive, heart1, heart2, heart3);
         }
     }
 
